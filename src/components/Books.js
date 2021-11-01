8fb6d00afb206fc5f6b2116fd24d52ef2dac5c93
ntzwilly <ntzwilly@gmail.com>
2021-11-01 14:02:12 +0200

    Add Books default view

--- src/components/Books.js
+++ src/components/Books.js
@@ -3,7 +3,23 @@ import React from "react";
 const Books = () => {
   return (
     <div>
-      <h1>Books</h1>
+       <ul className='Book-list'>
+           <li>
+               <h2>Book 1</h2>
+               <button>Remove</button>
+           </li>
+           <li>
+               <h2>Book 2</h2>
+               <button>Remove</button>
+           </li>
+       </ul>
+      <form>
+        <label>
+          ADD NEW BOOK:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="ADD BOOK" />
+      </form>
     </div>
   );
 };

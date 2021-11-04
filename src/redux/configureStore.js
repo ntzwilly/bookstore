fb2c39fcdcbcaeee1b373ff15307ae6e73ead968
ntzwilly <ntzwilly@gmail.com>
2021-11-04 14:18:27 +0200

    Fix bug

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import booksReducer from './books/books';
 
 const reducer = combineReducers({
   books: booksReducer,
 });
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 export default store;
